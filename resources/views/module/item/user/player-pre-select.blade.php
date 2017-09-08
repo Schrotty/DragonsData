cc59c0b5215804706fd434660178aefdcf986a86
Ruben Maurer <rubenmaurer@live.de>
2017-09-08 17:13:07 +0200

    Hotfix: Fixed user edit screen(Failed to load player character)

--- resources/views/module/item/user/player-pre-select.blade.php
+++ resources/views/module/item/user/player-pre-select.blade.php
@@ -1,4 +1,5 @@
-@foreach(\App\Item::byTag(\App\Settings::playerTag()) as $item)
+@php $items = \App\Item::getPlayer(\App\Settings::playerIdent()) @endphp
+@foreach($items as $item)
     @if(in_array($item->_id, (array)$user->chars))
         <option selected value="{{ $item->_id }}">{{ $item->name }}</option>
         @continue
